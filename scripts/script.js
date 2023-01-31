4c14d2181c07086e84066cd536f205b24cbd5eb3
Ivan Khachaturyan <i.s.khachaturyan@yandex.ru>
2023-01-31 15:17:25 +0300

    feat: Добавить карточки из массива

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,52 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+// Нашли переменную которую мы будем копировать(клонировать)
+const template = document.querySelector('#card-elements').content.querySelector('.element');
+
+// Нашли переменную В которую будем вкладывать карточки
+
+const elements = document.querySelector('.elements');
+
+// Создаем функцию для рендеринга карторчек 
+
+function renderCards () {
+    initialCards.forEach ((item) => {
+        const card = template.cloneNode(true);
+        card.querySelector('.element__title').textContent = item.name;
+        card.querySelector('.element__image').src = item.link;
+
+        elements.append(card)
+    })
+}
+
+renderCards();
+
+
+// Открытие попап ПР4
 const popupBtnopen = document.querySelector('.profile__button');
 const popupContainer = document.querySelector('.popup');
 const popupBtnclose = document.querySelector('.popup__close');
@@ -5,7 +54,8 @@ const profileName = document.querySelector('.profile__name');
 const profileText = document.querySelector('.profile__text');
 const nameInput = document.querySelector('.popup__text_form_name');
 const jobInput = document.querySelector('.popup__text_form_prof');
-const formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup__form'); 
+
 
 function openPopup () {
     popupContainer.classList.add("popup_opened");
@@ -19,6 +69,8 @@ function closePopup () {
 
 popupBtnopen.addEventListener('click', openPopup);
 popupBtnclose.addEventListener('click', closePopup);
+
+
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
