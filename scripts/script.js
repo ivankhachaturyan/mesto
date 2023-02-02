f93adbd7f46e9813b1deb5eec00b8dcf40927fd1
Ivan Khachaturyan <i.s.khachaturyan@yandex.ru>
2023-02-02 14:14:02 +0300

    feat: Добавить плавное всплытие popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // Открытие попап ПР4
 const popupBtnopen = document.querySelector('.profile__button');
 const popupContainer = document.querySelector('.popup_type_form');
@@ -8,6 +35,32 @@ const nameInput = document.querySelector('.popup__text_form_name');
 const jobInput = document.querySelector('.popup__text_form_prof');
 const formElement = document.querySelector('.popup__form_type_form'); 
 
+// Открытие попап для карточек. 
+
+const popupCardContainer = document.querySelector('.popup_type_card');
+const popupCardBtnopen = document.querySelector('.profile__add-button');
+const popupCardBtnclose = document.querySelector('.popup__close_type_card');
+
+const titleInput = document.querySelector('.popup__text_title_card');
+const linkInput = document.querySelector('.popup__text_link_card');
+
+// Нашли переменную которую мы будем копировать(клонировать)
+const template = document.querySelector('#card-elements').content.querySelector('.element');
+
+// Нашли переменную В которую будем вкладывать карточки
+const elements = document.querySelector('.elements');
+
+const formCardElement = document.querySelector('.popup__form_type_card');
+
+// Открытие попап для картинок.
+
+const popupImageContainer = document.querySelector('.popup_type_image');
+const popupImageBtnclose = document.querySelector('.popup__close_type_image');
+
+const popupImage = document.querySelector('.popup__image');
+const popupImgSubtitle = document.querySelector('.popup__subtitle');
+
+// Функции ПР4
 function openPopup () {
     popupContainer.classList.add("popup_opened");
     nameInput.value = profileName.textContent;
@@ -22,7 +75,6 @@ popupBtnopen.addEventListener('click', openPopup);
 popupBtnclose.addEventListener('click', closePopup);
 
 
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value; 
@@ -32,42 +84,7 @@ function handleFormSubmit (evt) {
 
 formElement.addEventListener('submit', handleFormSubmit); 
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-
-// Открытие попап для карточек. 
-
-const popupCardContainer = document.querySelector('.popup_type_card');
-const popupCardBtnopen = document.querySelector('.profile__add-button');
-const popupCardBtnclose = document.querySelector('.popup__close_type_card');
-
-const titleInput = document.querySelector('.popup__text_title_card');
-const linkInput = document.querySelector('.popup__text_link_card');
-
+// Функции ПР5
 function openPopupCard () {
     popupCardContainer.classList.add("popup_opened");
 }
@@ -79,12 +96,6 @@ function closePopupCard () {
 popupCardBtnopen.addEventListener('click', openPopupCard);
 popupCardBtnclose.addEventListener('click', closePopupCard);
 
-// Нашли переменную которую мы будем копировать(клонировать)
-const template = document.querySelector('#card-elements').content.querySelector('.element');
-
-// Нашли переменную В которую будем вкладывать карточки
-const elements = document.querySelector('.elements');
-
 // Создаем функцию для рендеринга карторчек 
 function renderCards () {
     const cards = initialCards.map ((item) => {
@@ -95,9 +106,7 @@ function renderCards () {
 
 renderCards();
 
-const formCardElement = document.querySelector('.popup__form_type_card'); 
-
-
+ 
 formCardElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
     const itemTitleInput = titleInput.value;
@@ -125,14 +134,6 @@ function createCard (item) {
 
   return card;
 }
-
-// Открытие попап для картинок.
-
-const popupImageContainer = document.querySelector('.popup_type_image');
-const popupImageBtnclose = document.querySelector('.popup__close_type_image');
-
-const popupImage = document.querySelector('.popup__image');
-const popupImgSubtitle = document.querySelector('.popup__subtitle');
 
 function openPopupImage (evt) {
   popupImageContainer.classList.add("popup_opened");
