e13f6256080a27bc9ccd0a4dd1a32a11acbe0f48
Ivan Khachaturyan <i.s.khachaturyan@yandex.ru>
2023-02-15 20:42:40 +0300

    feat: Добавить данные в валидацию форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,30 +1,31 @@
 
-//Проект работа 6
-configValidation({
-  // formSelector: '.popup__form',
-  // inputSelector: '.popup__input',
-  // submitButtonSelector: '.popup__button',
-  // inactiveButtonClass: 'popup__button_disabled',
-  // inputErrorClass: 'popup__input_type_error',
-  // errorClass: 'popup__error_visible'
-});
+//  // Проект работа 6
+const configValidation = ({
+  formSelector: '.popup__form',   
+  inputSelector: '.popup__text',
+  submitButtonSelector: '.popup__save',
+  inactiveButtonClass: 'popup__save_type_inactive',
+  inputErrorClass: 'popup__text_type_error',
+  errorClass: 'popup__input-error_active',
+  fieldsetSelector: '.popup__fieldset'
+}); 
 
  // Функция, которая добавляет класс с ошибкой
  const showInputError = (formElement, inputElement, errorMessage) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add('popup__text_type_error');
+    inputElement.classList.add(configValidation.inputErrorClass);
      // Заменим содержимое span с ошибкой на переданный параметр
      errorElement.textContent = errorMessage;
      // Показываем сообщение об ошибке
-     errorElement.classList.add('popup__input-error_active');
+     errorElement.classList.add(configValidation.errorClass);
  };
  
  // Функция, которая удаляет класс с ошибкой
  const hideInputError = (formElement, inputElement) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove('popup__text_type_error');
+    inputElement.classList.remove(configValidation.inputErrorClass);
      // Скрываем сообщение об ошибке
-     errorElement.classList.remove('popup__input-error_active');
+     errorElement.classList.remove(configValidation.errorClass);
       // Очистим ошибку
      errorElement.textContent = '';
  };
@@ -40,6 +41,8 @@ configValidation({
          hideInputError(formElement, inputElement);
      }
  };
+ 
+
 
   const hasInvalidInput = (inputList) => {
     return inputList.some ((inputElement)=>{
@@ -49,17 +52,17 @@ configValidation({
 
   const toggleButtonState = (inputList, buttonElement) => {
     if (hasInvalidInput(inputList)) {
-      buttonElement.classList.add('popup__save_type_inactive');
+      buttonElement.classList.add(configValidation.inactiveButtonClass);
       buttonElement.disabled = true;
     } else {
-      buttonElement.classList.remove('popup__save_type_inactive');
+      buttonElement.classList.remove(configValidation.inactiveButtonClass);
       buttonElement.disabled = false;
     }
   };
 
   const setEventListeners = (formElement) => {
-    const inputList = Array.from(formElement.querySelectorAll('.popup__text'));
-    const buttonElement = formElement.querySelector('.popup__save');
+    const inputList = Array.from(formElement.querySelectorAll(configValidation.inputSelector));
+    const buttonElement = formElement.querySelector(configValidation.submitButtonSelector);
     toggleButtonState(inputList, buttonElement);
     
     inputList.forEach ((inputElement) => {
@@ -71,12 +74,12 @@ configValidation({
   };
  
   const enableValidation = () => {
-    const formList = Array.from(document.querySelectorAll('.popup__form'));
+    const formList = Array.from(document.querySelectorAll(configValidation.formSelector));
     formList.forEach((formElement) => {
       formElement.addEventListener('submit', function (evt) {
         evt.preventDefault();
       });
-      const fieldsetList = Array.from(formElement.querySelectorAll('.popup__fieldset'));
+      const fieldsetList = Array.from(formElement.querySelectorAll(configValidation.fieldsetSelector));
       fieldsetList.forEach((fieldSet) => {
       setEventListeners(fieldSet);
   }); 
@@ -85,9 +88,8 @@ configValidation({
 
  enableValidation();
 
-//  очищает ошибки в popup
  function validationPopupErrors (valid) { 
-  const deleteErrors = Array.from(valid.querySelectorAll('.popup__text')); 
+  const deleteErrors = Array.from(valid.querySelectorAll(configValidation.inputSelector)); 
   deleteErrors.forEach(function (item) { 
     hideInputError(valid, item); 
   }); 
